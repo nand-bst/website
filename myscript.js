05083c02e1b7c89c950319860e341780459ff4af
Nand Kumar <nand.kumar@bluestacks.com>
2022-04-19 18:16:06 +0530

    Adding accordion in header

--- myscript.js
+++ myscript.js
@@ -9,4 +9,41 @@ var acc = document.getElementsByClassName("products");
     });
 }
 
+console.log(document.getElementsByClassName("hamburger-menu"));
+document.getElementsByClassName("hamburger-menu")[0].addEventListener("click",function(){
 
+	var menuhamburger = document.getElementsByClassName("hamburger-menu");
+	menuhamburger[0].style.display="none";
+
+	console.log(document.getElementsByClassName("hamburger-menu-close"));
+	document.getElementsByClassName("hamburger-menu-close")[0].style.display="block";
+
+	var leftsection = document.getElementsByClassName("header-leftsection");
+	console.log('hi');
+	console.log(leftsection);
+
+	leftsection[0].classList.toggle("open");
+
+	document.getElementsByClassName("footer")[0].classList.add("footer-add");
+
+})
+
+document.getElementsByClassName("hamburger-menu-close")[0].addEventListener("click",function(){
+	 document.getElementsByClassName("hamburger-menu-close")[0].style.display="none";
+	 document.getElementsByClassName("hamburger-menu")[0].style.display="block";
+	 document.getElementsByClassName("header-leftsection")[0].classList.remove("open");
+	 document.getElementsByClassName("footer")[0].classList.remove("footer-add");
+})
+
+
+var acc = document.getElementsByClassName("header-leftsection-items");
+    var i;
+    console.log(acc);
+  for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+  	console.log('active changed');
+
+    this.classList.toggle("active");
+    });
+
+}
